e6f14231d3b3d11a3992d52248213d3074fd90d4
AryaanSheth <avsheth03@gmail.com>
2024-12-09 19:21:19 +0000

    chore: Update example code

--- examples/main.go
+++ examples/main.go
@@ -7,20 +7,18 @@ import (
 	"github.com/AryaanSheth/gopsd"
 )
 
+// Example: Adding Multiple Filters
 func main() {
 	var gps *gopsd.Session
 	var err error
 
 	address := "localhost:2947"
 
-	fmt.Printf("Connecting to GPSD at %s\n", address)
 	if gps, err = gopsd.Dial(address); err != nil {
 		panic(fmt.Sprintf("Failed to connect to GPSD: %s", err))
 	}
 	defer gps.Close()
-	fmt.Println("Connected to GPSD")
 
-	fmt.Println("Adding filters")
 	gps.AddFilter("TPV", func(data interface{}) {
 		if tpv, ok := data.(*gopsd.TPV); ok {
 			fmt.Printf("TPV - Mode: %d, Time: %v\n", tpv.Mode, tpv.Time)
@@ -36,11 +34,8 @@ func main() {
 			fmt.Printf("SKY - %v\n", data)
 		}
 	})
-	fmt.Println("Filters added")
 
-	fmt.Println("Starting watch")
 	done := gps.Watch()
-	fmt.Println("Watch started")
 
 	select {
 	case <-done:
