e5d7cef4d4e1f2c49d3a7e35f8523db359457393
AryaanSheth <avsheth03@gmail.com>
2024-12-09 18:08:00 +0000

    fix: Updates example code with new gopsd changes

--- examples/main.go
+++ examples/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"time"
 
@@ -14,31 +13,39 @@ func main() {
 
 	address := "localhost:2947"
 
+	fmt.Printf("Connecting to GPSD at %s\n", address)
 	if gps, err = gopsd.Dial(address); err != nil {
 		panic(fmt.Sprintf("Failed to connect to GPSD: %s", err))
 	}
 	defer gps.Close()
+	fmt.Println("Connected to GPSD")
 
-	gps.AddFilter("TPV", func(data []byte) {
-		var tpv gopsd.TPV
-		if err := json.Unmarshal(data, &tpv); err == nil {
+	fmt.Println("Adding filters")
+	gps.AddFilter("TPV", func(data interface{}) {
+		if tpv, ok := data.(*gopsd.TPV); ok {
 			fmt.Printf("TPV - Mode: %d, Time: %v\n", tpv.Mode, tpv.Time)
+		} else {
+			fmt.Printf("TPV - %v\n", data)
 		}
 	})
 
-	gps.AddFilter("SKY", func(data []byte) {
-		var sky gopsd.SKY
-		if err := json.Unmarshal(data, &sky); err == nil {
+	gps.AddFilter("SKY", func(data interface{}) {
+		if sky, ok := data.(*gopsd.SKY); ok {
 			fmt.Printf("SKY - %d satellites\n", len(sky.Satellites))
+		} else {
+			fmt.Printf("SKY - %v\n", data)
 		}
 	})
+	fmt.Println("Filters added")
 
+	fmt.Println("Starting watch")
 	done := gps.Watch()
+	fmt.Println("Watch started")
 
 	select {
 	case <-done:
 		fmt.Println("GPSD connection closed")
-	case <-time.After(5 * time.Second):
+	case <-time.After(30 * time.Second):
 		fmt.Println("Connection timeout")
 		gps.Close()
 	}
